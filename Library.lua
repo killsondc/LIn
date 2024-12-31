af3b2c35fba64fdfe4c0a0ec54ab9e64579709d5
killsondc <104129739+killsondc@users.noreply.github.com>
2024-12-31 05:12:17 -0600

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -150,6 +150,7 @@ function Library:CreateLabel(Properties, IsHud)
         TextColor3 = Library.FontColor;
         TextSize = 16;
         TextStrokeTransparency = 0;
+        RichText = true;
     });
 
     Library:ApplyTextStroke(_Instance);
