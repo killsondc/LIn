1a2363173bd85f67467591bfd80f3593ed2d11a5
killsondc <104129739+killsondc@users.noreply.github.com>
2025-05-22 19:33:51 -0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1861,6 +1861,7 @@ do
             Position = UDim2.new(1, 6, 0, 0);
             TextSize = 14;
             Text = Info.Text;
+            RichText = true;
             TextXAlignment = Enum.TextXAlignment.Left;
             ZIndex = 6;
             Parent = ToggleInner;
