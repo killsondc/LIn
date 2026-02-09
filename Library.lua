c4b05014ca00ad603837aca791d3a74c8baad5a1
killsondc <104129739+killsondc@users.noreply.github.com>
2026-02-08 21:12:26 -0600

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2893,6 +2893,7 @@ function Library:Notify(Text, Time)
         Position = UDim2.new(0, 4, 0, 0);
         Size = UDim2.new(1, -4, 1, 0);
         Text = Text;
+        RichText = true;
         TextXAlignment = Enum.TextXAlignment.Left;
         TextSize = 14;
         ZIndex = 103;
